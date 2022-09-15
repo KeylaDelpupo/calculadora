bc7020d1d93297f2d91189bfef77bb161b4f34d3
ky.agro@gmail.com <ky.agro@gmail.com>
2022-09-15 20:51:28 -0300

    primeiro commit

---
+++ calculos.js
@@ -0,0 +1,19 @@
+const somar (a,b) {
+    return a + b;
+},
+const subtrair (a,b){
+    return a-b;
+},
+const multiplicar (a,b){
+    return a*b;
+},
+const dividir (a,b){
+        return a / b;
+},
+
+const calculadora (a, b, operacao){
+    return operacao
+},
+
+console.log(calculadora(3,4,somar));
+console.log(calculadora(3,4,multiplicar));
